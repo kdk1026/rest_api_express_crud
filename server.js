89c04c4693a545a2b55a3a5014873147410cc332
kdk <kdk1026@gmail.com>
2021-05-06 01:37:49 +0900

    cors 적용

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const router = require('./routes/index');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use(router);
 
